d421e9833a2ec94a151b62988faf53ae45b4a042
Justin Cromer <justincromer@protonmail.com>
2024-12-08 17:09:12 -0800

    about to try something wonky

--- cmd/day_three/main.go
+++ cmd/day_three/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/big"
 	"os"
 	"regexp"
@@ -71,17 +72,49 @@ func solvePartTwo(contents []string) *big.Int {
 		return big.NewInt(int64(a * b))
 	}
 
-	// doRegex := regexp.MustCompile(`do()`)
-	// dontRegex := regexp.MustCompile(`don't()`)
-	mulRegex := regexp.MustCompile(`don't.+` + opName + `\(\d+,\d+\)`)
+	doRegex := regexp.MustCompile(`do\(\)`)
+	dontRegex := regexp.MustCompile(`don't\(\)`)
+	mulRegex := regexp.MustCompile(opName + `\(\d+,\d+\)`)
 	numRegex := regexp.MustCompile(`\d+`)
 
 	sumTotal := big.NewInt(0)
+	okayUntil := math.MaxInt
+	okayAfter := 0
 	for _, line := range contents {
-		matches := mulRegex.FindAllString(line, -1)
-		fmt.Printf("Matches: %v\n", matches)
+		mulMatches := mulRegex.FindAllIndex([]byte(line), -1)
+		doMatches := doRegex.FindAllIndex([]byte(line), -1)
+		dontMatches := dontRegex.FindAllIndex([]byte(line), -1)
 
-		for _, m := range matches {
+		if len(dontMatches) > 0 {
+			okayUntil = dontMatches[0][0]
+		}
+		if len(doMatches) > 0 {
+			okayAfter = doMatches[0][0]
+		}
+
+		for mul, do, dont := 0, 0, 0; mul < len(mulMatches); mul++ {
+			start, stop := mulMatches[mul][0], mulMatches[mul][1]
+			if start > okayUntil && start < okayAfter {
+				continue
+			} 
+
+			if start < okayUntil && okayUntil > okayAfter {
+				do++
+				if len(doMatches) > do {
+					okayAfter = doMatches[do][0]
+				} else {
+					okayAfter = math.MaxInt
+				}
+			} else if start > okayAfter && okayAfter > okayUntil {
+				dont++
+				if len(dontMatches) > dont {
+					okayUntil = dontMatches[dont][0]
+				} else {
+					okayUntil = math.MaxInt
+				}
+			}
+
+			m := line[start:stop]
 			numbers := numRegex.FindAllString(m, -1)
 			a, err := strconv.Atoi(numbers[0])
 			if err != nil {
